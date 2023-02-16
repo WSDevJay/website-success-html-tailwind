c0055f84bdff7575be78adc4629d78e2e0a18444
Jay <jay@websitesuccess.co.uk>
2023-02-17 03:57:15 +0800

    header and footer completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/marketplace_builder/html/**/*.{html,js}"],
+  content: ["./marketplace_builder/html/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Nunito Sans'],
+        sans: ["Nunito Sans"],
       },
       colors: {
-        'primary-yellow': '#e8e20c',
+        "primary-yellow": "#e8e20c",
+        "primary-blue": "#31A7E0",
+        "primary-dark": "#313D47",
+        "primary-light": "#EBECEC",
+      },
+      maxWidth: {
+        "screen-base": "1320px",
       },
     },
   },
-  plugins: [],
-}
+};
+
+/* npx tailwindcss -i ./src/input.css -o ./marketplace_builder/assets/css/output.css --watch */
